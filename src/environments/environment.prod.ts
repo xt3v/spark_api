365f09a688d2c65c9d44689a94d286e91a48cd46
Norman Munge <normanmunge@gmail.com>
2021-02-17 12:03:04 +0300

    Fixes environment:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 //TODO-> Pull the environments from a dotenv for security purposes
 export const environment = {
-  production: true,
-  APIEndpoint: 'https://bwsapi.sisitech.dev/',
-  APIv1Endpoint: 'https://bwsapi.sisitech.dev/api/v1/',
+  production: false,
+  APIEndpoint: 'https://api.spark.sisitech.dev/',
+  APIv1Endpoint: 'https://api.spark.sisitech.dev/api/v1/',
   APIClientID : 'fbaPXGrD6wewVEqoOkJfvierIrYbnROPXMa8CDv5'
 }
