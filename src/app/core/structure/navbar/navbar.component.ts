d7cda39c7069f8cc1926bd36fe19fc4354bce6a9
Norman Munge <normanmunge@gmail.com>
2021-03-04 17:01:01 +0300

    Adds signout in navbar

--- src/app/core/structure/navbar/navbar.component.ts
+++ src/app/core/structure/navbar/navbar.component.ts
@@ -9,25 +9,12 @@ import { AuthService } from 'src/app/services';
 })
 export class NavbarComponent implements OnInit {
 
-  public hoveredSubElement: boolean = false;
-  public hoveredSubsubElement: boolean = false;
-
   constructor(
     private _route:Router,
     private _authService: AuthService
   ) { }
 
   ngOnInit(): void {
-  }
-
-  toggleMouseOverSubElement() {
-    this.hoveredSubElement = !this.hoveredSubElement;
-
-    //TODO :-> Define better way to handle hovering and focus of the sub sub items and closing of the nav sub items when mouse clicked outside element.
-    this.hoveredSubsubElement = false;
-  }
-  toggleMouseOverSubsubElement() {
-    this.hoveredSubsubElement = !this.hoveredSubsubElement;
   }
 
   signOut() {
