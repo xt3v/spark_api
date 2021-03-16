bcfd29df20ca095689acc9d54dbd8f521c8474c8
Chiama <shawn.chiama@strathmore.edu>
2021-03-16 12:03:39 +0300

    Initial setup reusable toast notification components

---
+++ src/app/shared/toast-notifications/toast-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-notifications',
+  templateUrl: './toast-notifications.component.html',
+  styleUrls: ['./toast-notifications.component.scss']
+})
+export class ToastNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
