095c948598a128e94bb11f42810cb04ffb22e22b
Chiama <shawn.chiama@strathmore.edu>
2021-04-20 14:25:49 +0300

    Fixed Stockroom Breadcrumbs

--- src/app/modules/stockroom/stockroom-routing.module.ts
+++ src/app/modules/stockroom/stockroom-routing.module.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AddStockroomComponent } from './add/add-stockroom.component';
 import { StockroomListComponent } from './list/stockroom-list.component';
 export const StockroomRoutes: Routes = [
-    { path: '', redirectTo: 'add', pathMatch: 'full' },
+    { path: '', redirectTo: 'list', pathMatch: 'full' },
     { path: 'add', component: AddStockroomComponent, data: { breadcrumb: 'Add Stockroom' } },
     { path: 'list', component: StockroomListComponent, data: { breadcrumb: 'List Stockrooms' } },
     
